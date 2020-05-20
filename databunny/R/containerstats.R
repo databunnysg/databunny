29d88a0ed5ecc1209be9b52453c67cae582ce5d4
databunnysg <yi@databunny.sg>
2020-05-20 17:04:07 +0000

    initial commit - databunny cpu/memory/limit for docker container rstudio sessions

---
+++ databunny/R/containerstats.R
@@ -0,0 +1,36 @@
+library(readr)
+library(miniUI)
+library(shiny)
+
+
+#' Query CPU and Memory usage and limit by docker container and show it on a Viewer Pane
+#'
+#' @export
+#'
+updatecpumemstats<-function(){
+
+
+  ui<-miniPage({
+    gadgetTitleBar("Container Status")
+    miniContentPanel(
+      tableOutput("stats")
+    )
+  })
+  server<-function(input, output, session) {
+    observe({
+      #invalidateLater(2000)
+      memlimit<-system("cat /sys/fs/cgroup/memory/memory.limit_in_bytes",intern = TRUE)
+      memused<-system("cat /sys/fs/cgroup/memory/memory.usage_in_bytes",intern = TRUE)
+      cpu<-system("top -bn1",intern = TRUE)
+      cpudt<-read_table(cpu[-(1:6)])
+      cpuusage<-paste0(sum(cpudt$`%CPU`),"%")
+      statsdt<-data.frame("MemoryUsed"=paste(round(as.numeric(memused)/(1024*1024*1024),digits = 2),"GB"),"MemoryLimit"=paste(round(as.numeric(memlimit)/(1024*1024*1024),digits = 2),"GB"),"CPUUsage"=cpuusage)
+      output$stats<-renderTable(statsdt)
+    })
+    observeEvent(input$done, {
+      stopApp()
+    })
+  }
+  viewer <- paneViewer()
+  runGadget(ui, server, viewer = viewer)
+}
