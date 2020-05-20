d30ca8a9aafdb6b53d5c4b74eef2a9da83840847
databunnysg <yi@databunny.sg>
2020-05-20 17:47:52 +0000

    additional function added

--- R/containerstats.R
+++ R/containerstats.R
@@ -4,6 +4,7 @@ library(shiny)
 
 
 #' Query CPU and Memory usage and limit by docker container and show it on a Viewer Pane
+#' 
 #' This is rstudio addin function, the addin will be automatically added to rstudio once packege was installed
 #' Click Addins -> Databunny -> "Show CPU Memory Usage Limit" to activate addin
 #' @export
@@ -19,7 +20,7 @@ updatecpumemstats<-function(){
   })
   server<-function(input, output, session) {
     observe({
-      #invalidateLater(2000)
+      invalidateLater(1000)
       memlimit<-system("cat /sys/fs/cgroup/memory/memory.limit_in_bytes",intern = TRUE)
       memused<-system("cat /sys/fs/cgroup/memory/memory.usage_in_bytes",intern = TRUE)
       cpu<-system("top -bn1",intern = TRUE)
@@ -34,4 +35,36 @@ updatecpumemstats<-function(){
   }
   viewer <- paneViewer()
   runGadget(ui, server, viewer = viewer)
+}
+
+#' Query CPU usage from OS
+#' 
+#' This is Linux command
+#' @export
+#'
+getCPUUsage<-function(){
+  cpu<-system("top -bn1",intern = TRUE)
+  cpudt<-read_table(cpu[-(1:6)])
+  cpuusage<-paste0(sum(cpudt$`%CPU`),"%")
+  return(cpuusage)
+}
+
+#' Query Memory usage from OS
+#' 
+#' This is Linux command
+#' @export
+#'
+getMemoryUsage<-function(){
+  memused<-system("cat /sys/fs/cgroup/memory/memory.usage_in_bytes",intern = TRUE)
+  return(memused)
+}
+
+#' Query Memory Limit from OS
+#' 
+#' This is Linux command
+#' @export
+#'
+getMemoryLimit<-function(){
+  memlimit<-system("cat /sys/fs/cgroup/memory/memory.limit_in_bytes",intern = TRUE)
+  return(memlimit)
 }
