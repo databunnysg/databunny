08f7dd3ed199bfa61b147bee69d1b197c7e0ef80
databunnysg <yi@databunny.sg>
2020-05-22 23:38:49 +0000

    add windows os support

--- R/containerstats.R
+++ R/containerstats.R
@@ -16,7 +16,8 @@ usageaddins<-function(){
   server<-function(input, output, session) {
     shiny::observe({
       shiny::invalidateLater(1000)
-      memlimit<-system("cat /sys/fs/cgroup/memory/memory.limit_in_bytes",intern = TRUE)
+      memlimit<-min(as.numeric(system("cat /sys/fs/cgroup/memory/memory.limit_in_bytes",intern = TRUE)),as.numeric(system("awk '/MemTotal/ {print $2}' /proc/meminfo", intern=TRUE))*1024)
+      as.numeric(system("awk '/MemTotal/ {print $2}' /proc/meminfo", intern=TRUE))
       memused<-system("cat /sys/fs/cgroup/memory/memory.usage_in_bytes",intern = TRUE)
       cpu<-system("top -bn1",intern = TRUE)
       cpudt<-readr::read_table(cpu[-(1:6)])
@@ -38,9 +39,19 @@ usageaddins<-function(){
 #' @export
 #'
 getCPUUsage<-function(){
-  cpu<-system("top -bn1",intern = TRUE)
-  cpudt<-readr::read_table(cpu[-(1:6)])
-  cpuusage<-paste0(sum(cpudt$`%CPU`),"%")
+  linux <- Sys.info()['sysname'] == "Linux"
+  windows <- Sys.info()['sysname'] == "Windows"
+  if(linux)
+  {
+    cpu<-system("top -bn1",intern = TRUE)
+    cpudt<-readr::read_table(cpu[-(1:6)])
+    cpuusage<-paste0(sum(cpudt$`%CPU`),"%")  
+  }
+  if(windows)
+  {
+    cpuusage<-as.numeric(gsub("\r","",gsub("LoadPercentage=","",system('wmic cpu get LoadPercentage /Value',intern=TRUE)[3])))
+  }
+  
   return(cpuusage)
 }
 
@@ -50,7 +61,19 @@ getCPUUsage<-function(){
 #' @export
 #'
 getMemoryUsage<-function(){
-  memused<-system("cat /sys/fs/cgroup/memory/memory.usage_in_bytes",intern = TRUE)
+  linux <- Sys.info()['sysname'] == "Linux"
+  windows <- Sys.info()['sysname'] == "Windows"
+  if(linux)
+  {
+    memused<-round(as.numeric(system("cat /sys/fs/cgroup/memory/memory.usage_in_bytes",intern = TRUE))/(1024*1024*1024),digit=1)
+  }
+  if(windows)
+  {
+    totalmem<-as.numeric(gsub("\r","",gsub("TotalVisibleMemorySize=","",system('wmic OS get TotalVisibleMemorySize /Value',intern=TRUE)[3])))  
+    availmem<-as.numeric(gsub("\r","",gsub("FreePhysicalMemory=","",system('wmic OS get FreePhysicalMemory /Value',intern=TRUE)[3])))
+    memused<-round((totalmem-availmem)/(1024*1024),digits = 1)
+  }
+  
   return(memused)
 }
 
@@ -60,6 +83,16 @@ getMemoryUsage<-function(){
 #' @export
 #'
 getMemoryLimit<-function(){
-  memlimit<-system("cat /sys/fs/cgroup/memory/memory.limit_in_bytes",intern = TRUE)
+  linux <- Sys.info()['sysname'] == "Linux"
+  windows <- Sys.info()['sysname'] == "Windows"
+  if(linux)
+  {
+    memlimit<-round(min(as.numeric(system("cat /sys/fs/cgroup/memory/memory.limit_in_bytes",intern = TRUE)),as.numeric(system("awk '/MemTotal/ {print $2}' /proc/meminfo", intern=TRUE))*1024)/(1024*1024*1024),digits = 1)
+  }
+  if(windows)  
+  {
+    memlimit<-round(as.numeric(gsub("\r","",gsub("TotalVisibleMemorySize=","",system('wmic OS get TotalVisibleMemorySize /Value',intern=TRUE)[3])))/1024/1024 , digits=1)
+  }
+  
   return(memlimit)
 }
